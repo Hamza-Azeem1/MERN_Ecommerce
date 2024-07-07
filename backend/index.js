5e6d3c5030995b6f10518c0deee998b54c8f5cff
Hamza-Azeem1 <hamzaazeem023@gmail.com>
2024-07-07 22:29:13 +0500

    updated

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,14 @@
-const express = require('express')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-require('dotenv').config()
-const connectDB = require('./config/db')
-const router = require('./routes')
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+const connectDB = require('./config/db');
+const router = require('./routes');
 
-const app = express()
-
-// app.use(cors({
-//     origin: process.env.FRONTEND_URL,
-//     credentials: true
-// }))
+const app = express();
 
 const corsOptions = {
-    origin: ['https://myshop.vercel.app'],
+    origin: 'https://myshop.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
     credentials: true,
@@ -25,16 +20,16 @@ app.use(cors(corsOptions));
 // Handle preflight requests
 app.options('*', cors(corsOptions));
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
-app.use("/api", router)
+app.use("/api", router);
 
-const PORT = 8000 || process.env.PORT
+const PORT = process.env.PORT || 8000;
 
 connectDB().then(() => {
     app.listen(PORT, () => {
-        console.log("DB connected!")
-        console.log(`Server is started at Port:${PORT}...`)
-    })
-})
+        console.log("DB connected!");
+        console.log(`Server is started at Port:${PORT}...`);
+    });
+});
