f067b5222008bc5613b2498650a7d6c9572bb764
Hamza-Azeem1 <hamzaazeem023@gmail.com>
2024-07-11 17:35:09 +0500

    updated

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,19 +1,18 @@
-import { useContext, useEffect, useState } from "react"
-import SummaryApi from "../common"
-import Context from "../context"
-import displayPKRcurrency from "../helpers/displayCurrency"
+import { useContext, useEffect, useState } from 'react'
+import SummaryApi from '../common'
+import Context from '../context'
+import displayPKRcurrency from '../helpers/displayCurrency'
 import { MdDelete } from "react-icons/md";
 
 const Cart = () => {
-
     const [data, setData] = useState([])
     const [loading, setLoading] = useState(false)
-
     const context = useContext(Context)
-    const loadingCart = new Array(context.cartProductCount).fill(null)
+    const loadingCart = new Array(4).fill(null)
+
 
     const fetchData = async () => {
-        setLoading(true)
+
         const response = await fetch(SummaryApi.addToCartProductView.url, {
             method: SummaryApi.addToCartProductView.method,
             credentials: 'include',
@@ -21,18 +20,27 @@ const Cart = () => {
                 "content-type": 'application/json'
             },
         })
-        setLoading(false)
+
 
         const responseData = await response.json()
 
         if (responseData.success) {
             setData(responseData.data)
         }
+
+
+    }
+
+    const handleLoading = async () => {
+        await fetchData()
     }
 
     useEffect(() => {
-        fetchData()
+        setLoading(true)
+        handleLoading()
+        setLoading(false)
     }, [])
+
 
     const increaseQty = async (id, qty) => {
         const response = await fetch(SummaryApi.updateCartProduct.url, {
@@ -41,20 +49,24 @@ const Cart = () => {
             headers: {
                 "content-type": 'application/json'
             },
-            body: JSON.stringify({
-                _id: id,
-                quantity: qty + 1
-            })
+            body: JSON.stringify(
+                {
+                    _id: id,
+                    quantity: qty + 1
+                }
+            )
         })
 
         const responseData = await response.json()
+
 
         if (responseData.success) {
             fetchData()
         }
     }
 
-    const decreasQty = async (id, qty) => {
+
+    const decraseQty = async (id, qty) => {
         if (qty >= 2) {
             const response = await fetch(SummaryApi.updateCartProduct.url, {
                 method: SummaryApi.updateCartProduct.method,
@@ -62,13 +74,16 @@ const Cart = () => {
                 headers: {
                     "content-type": 'application/json'
                 },
-                body: JSON.stringify({
-                    _id: id,
-                    quantity: qty - 1
-                })
+                body: JSON.stringify(
+                    {
+                        _id: id,
+                        quantity: qty - 1
+                    }
+                )
             })
 
             const responseData = await response.json()
+
 
             if (responseData.success) {
                 fetchData()
@@ -83,9 +98,11 @@ const Cart = () => {
             headers: {
                 "content-type": 'application/json'
             },
-            body: JSON.stringify({
-                _id: id,
-            })
+            body: JSON.stringify(
+                {
+                    _id: id,
+                }
+            )
         })
 
         const responseData = await response.json()
@@ -98,13 +115,13 @@ const Cart = () => {
 
     const totalQty = data.reduce((previousValue, currentValue) => previousValue + currentValue.quantity, 0)
     const totalPrice = data.reduce((preve, curr) => preve + (curr.quantity * curr?.productId?.sellingPrice), 0)
-
     return (
-        <div className="container mx-auto">
-            <div className="text-center text-lg my-3">
+        <div className='container mx-auto'>
+
+            <div className='text-center text-lg my-3'>
                 {
                     data.length === 0 && !loading && (
-                        <p className="bg-white py-5">No Product in Cart!</p>
+                        <p className='bg-white py-5'>No Data</p>
                     )
                 }
             </div>
@@ -120,10 +137,11 @@ const Cart = () => {
                                     </div>
                                 )
                             })
+
                         ) : (
                             data.map((product, index) => {
                                 return (
-                                    <div key={index} className='w-full bg-white h-32 my-2 border border-slate-300  rounded grid grid-cols-[128px,1fr]'>
+                                    <div key={product?._id + "Add To Cart Loading" + index} className='w-full bg-white h-32 my-2 border border-slate-300  rounded grid grid-cols-[128px,1fr]'>
                                         <div className='w-32 h-32 bg-slate-200'>
                                             <img src={product?.productId?.productImage[0]} className='w-full h-full object-scale-down mix-blend-multiply' />
                                         </div>
@@ -132,16 +150,17 @@ const Cart = () => {
                                             <div className='absolute right-0 text-red-600 rounded-full p-2 hover:bg-red-600 hover:text-white cursor-pointer' onClick={() => deleteCartProduct(product?._id)}>
                                                 <MdDelete />
                                             </div>
+
                                             <h2 className='text-lg lg:text-xl text-ellipsis line-clamp-1'>{product?.productId?.productName}</h2>
                                             <p className='capitalize text-slate-500'>{product?.productId.category}</p>
                                             <div className='flex items-center justify-between'>
                                                 <p className='text-red-600 font-medium text-lg'>{displayPKRcurrency(product?.productId?.sellingPrice)}</p>
-                                                <p className='text-slate-600 font-semibold text-lg line-through'>{displayPKRcurrency(product?.productId?.price * product?.quantity)}</p>
+                                                <p className='text-slate-600 font-semibold text-lg'>{displayPKRcurrency(product?.productId?.sellingPrice * product?.quantity)}</p>
                                             </div>
-                                            <div className='flex items-center justify-center gap-3 mt-1'>
-                                                <button className='border border-red-600 text-red-600 hover:bg-red-600 hover:text-white w-6 h-6 flex justify-center items-center rounded' onClick={() => decreasQty(product?._id, product?.quantity)}>-</button>
+                                            <div className='flex items-center gap-3 mt-1'>
+                                                <button className='border border-red-600 text-red-600 hover:bg-red-600 hover:text-white w-6 h-6 flex justify-center items-center rounded ' onClick={() => decraseQty(product?._id, product?.quantity)}>-</button>
                                                 <span>{product?.quantity}</span>
-                                                <button className='border border-red-600 text-red-600 hover:bg-red-600 hover:text-white w-6 h-6 flex justify-center items-center rounded' onClick={() => increaseQty(product?._id, product?.quantity)}>+</button>
+                                                <button className='border border-red-600 text-red-600 hover:bg-red-600 hover:text-white w-6 h-6 flex justify-center items-center rounded ' onClick={() => increaseQty(product?._id, product?.quantity)}>+</button>
                                             </div>
                                         </div>
                                     </div>
@@ -152,11 +171,12 @@ const Cart = () => {
                 </div>
 
 
-                {/* Summary */}
+                {/***summary  */}
                 <div className='mt-5 lg:mt-0 w-full max-w-sm'>
                     {
                         loading ? (
                             <div className='h-36 bg-slate-200 border border-slate-300 animate-pulse'>
+
                             </div>
                         ) : (
                             <div className='h-36 bg-white'>
@@ -172,13 +192,14 @@ const Cart = () => {
                                 </div>
 
                                 <button className='bg-blue-600 p-2 text-white w-full mt-2'>Payment</button>
+
                             </div>
                         )
                     }
                 </div>
             </div>
-
         </div>
     )
 }
+
 export default Cart
